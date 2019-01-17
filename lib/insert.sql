a1d0c54d46c130d7470c290a823f15441b28dbde
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-17 17:30:08 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,3 +1,8 @@
 INSERT INTO bears VALUES (1,"Mr. Chocolate", 20, "M", "dark brown", "calm", 1);
-INSERT INTO  VALUES ( )
-INSERT INTO  VALUES
+INSERT INTO bears VALUES (2,"Rowdy", 3, "M",  )
+INSERT INTO bears VALUES (3,"Tabitha", 5, "M", )
+INSERT INTO bears VALUES (4,"Sergeant Brown", 6, "F", )
+INSERT INTO bears VALUES (5,"Melissa", 25, "F", )
+INSERT INTO bears VALUES (6,"Grinch", 15, "M",)
+INSERT INTO bears VALUES (7,"Wendy", 13, "F", )
+INSERT INTO bears VALUES (8,"null", 9, "F", )
