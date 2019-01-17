719bd401d7dec5f2e62b514f4780cdffb38f1e52
Ariel Hicks <hicks.ariel@gmail.com>
2019-01-17 17:32:28 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears VALUES (4,"Sergeant Brown", 6, "F", "green", "calm", 1 );
 INSERT INTO bears VALUES (5,"Melissa", 25, "F", "red", "calm", 1);
 INSERT INTO bears VALUES (6,"Grinch", 15, "M", "white", "calm", 1);
 INSERT INTO bears VALUES (7,"Wendy", 13, "F", "white", "calm", 1);
-INSERT INTO bears VALUES (8,"nill", 9, "F", "white", "calm", 1)
+INSERT INTO bears VALUES (8,"nil", 9, "F", "white", "calm", 1)
